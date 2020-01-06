924f15d02be5aaddc85f25a814ba6a6b5b2d9263
mostafaelsayed <mostafa.elsayed@ibm.com>
2020-01-07 01:29:29 +0200

    change in the connection to the db

--- database/models/index.js
+++ database/models/index.js
@@ -9,11 +9,12 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.url, config);
-}
+// if (process.env.NODE_ENV) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.url, config);
+// }
+sequelize = new Sequelize(config.url, config);
 
 fs
   .readdirSync(__dirname)
